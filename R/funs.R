1644f1e9794a5f010311f182d7bafadee7ac5cf9
Garrick Aden-Buie <garrick@adenbuie.com>
2022-01-10 12:36:12 -0500

    create demo package

---
+++ R/funs.R
@@ -0,0 +1,21 @@
+#' A
+#' @export
+a <- function() {}
+
+#' B
+#' @export
+b <- function() {}
+
+#' C
+#' @export
+c <- function() {}
+
+#' D
+#' @usage
+#' \special{?topic}
+#' @export
+`?` <- function() {}
+
+#' E
+#' @name e
+NULL
